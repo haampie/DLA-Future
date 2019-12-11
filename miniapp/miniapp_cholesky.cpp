4684009b8e0397011ec9ba6566da9c4c8029363c
Alberto Invernizzi <invernizzi@cscs.ch>
2019-12-11 17:11:35 +0100

    add basic miniapps structure with first empty miniapp

---
+++ miniapp/miniapp_cholesky.cpp
@@ -0,0 +1,16 @@
+//
+// Distributed Linear Algebra with Future (DLAF)
+//
+// Copyright (c) 2018-2019, ETH Zurich
+// All rights reserved.
+//
+// Please, refer to the LICENSE file in the root directory.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#include <iostream>
+
+int main() {
+  std::cout << "MiniAPP Cholesky" << std::endl;
+  return 0;
+}
