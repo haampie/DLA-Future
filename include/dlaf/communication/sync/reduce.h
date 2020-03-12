f344869a070a42daa284c39136b9404df34bb39f
Alberto Invernizzi <invernizzi@cscs.ch>
2020-03-12 14:06:41 +0100

    adapt to new name convention

--- include/dlaf/communication/sync/reduce.h
+++ include/dlaf/communication/sync/reduce.h
@@ -13,7 +13,7 @@
 /// @file
 
 #include <mpi.h>
-#include "dlaf/common/buffer_basic.h"
+#include "dlaf/common/data_descriptor.h"
 #include "dlaf/communication/communicator.h"
 #include "dlaf/communication/message.h"
 
@@ -27,15 +27,15 @@ namespace reduce {
 /// @brief MPI_Reduce wrapper (sender side)
 /// MPI Reduce(see MPI documentation for additional info)
 /// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator
-template <class BufferIn, class BufferOut>
-void collector(Communicator& communicator, MPI_Op reduce_operation, BufferIn input, BufferOut output) {
-  using T = std::remove_const_t<typename common::buffer_traits<BufferIn>::element_t>;
+template <class DataIn, class DataOut>
+void collector(Communicator& communicator, MPI_Op reduce_operation, DataIn input, DataOut output) {
+  using T = std::remove_const_t<typename common::data_traits<DataIn>::element_t>;
 
-  common::BufferWithMemory<T> tmp_mem_input;
-  common::BufferWithMemory<T> tmp_mem_output;
+  common::Buffer<T> tmp_mem_input;
+  common::Buffer<T> tmp_mem_output;
 
-  common::BufferBasic<const T> tmp_in = input;
-  common::BufferBasic<T> tmp_out = output;
+  common::DataDescriptor<const T> tmp_in = input;
+  common::DataDescriptor<T> tmp_out = output;
 
   // if input is not contiguous, copy it in a contiguous temporary buffer
   if (!input.is_contiguous()) {
@@ -48,7 +48,7 @@ void collector(Communicator& communicator, MPI_Op reduce_operation, BufferIn inp
     tmp_out = tmp_mem_output = common::create_temporary_buffer(tmp_out);
 
   auto message_input = comm::make_message(std::move(tmp_in));
-  auto message_output = comm::make_message(BufferOut(tmp_out));
+  auto message_output = comm::make_message(DataOut(tmp_out));
 
   MPI_Reduce(message_input.data(), message_output.data(), message_input.count(),
              message_input.mpi_type(), reduce_operation, communicator.rank(), communicator);
@@ -62,13 +62,13 @@ void collector(Communicator& communicator, MPI_Op reduce_operation, BufferIn inp
 /// MPI Reduce(see MPI documentation for additional info)
 /// @param rank_root  the rank that will collect the result in output
 /// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator
-template <class BufferIn>
-void participant(int rank_root, Communicator& communicator, MPI_Op reduce_operation, BufferIn input) {
-  using T = std::remove_const_t<typename common::buffer_traits<BufferIn>::element_t>;
+template <class DataIn>
+void participant(int rank_root, Communicator& communicator, MPI_Op reduce_operation, DataIn input) {
+  using T = std::remove_const_t<typename common::data_traits<DataIn>::element_t>;
 
-  common::BufferWithMemory<T> tmp_mem_input;
+  common::Buffer<T> tmp_mem_input;
 
-  common::BufferBasic<const T> tmp_in = input;
+  common::DataDescriptor<const T> tmp_in = input;
 
   // if input is not contiguous, copy it in a contiguous temporary buffer
   if (!input.is_contiguous()) {
@@ -89,9 +89,9 @@ void participant(int rank_root, Communicator& communicator, MPI_Op reduce_operat
 /// MPI Reduce(see MPI documentation for additional info)
 /// @param rank_root  the rank that will collect the result in output
 /// @param reduce_operation MPI_Op to perform on @p input data coming from ranks in @p communicator
-template <class BufferIn, class BufferOut>
-void reduce(const int rank_root, Communicator& communicator, MPI_Op reduce_operation, BufferIn input,
-            BufferOut output) {
+template <class DataIn, class DataOut>
+void reduce(const int rank_root, Communicator& communicator, MPI_Op reduce_operation, DataIn input,
+            DataOut output) {
   if (rank_root == communicator.rank())
     internal::reduce::collector(communicator, reduce_operation, input, output);
   else
